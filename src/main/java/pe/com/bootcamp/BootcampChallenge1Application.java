9d553db5bfa31bc68d5432258622648b31e307c3
jgsistem <jgsistem@gmail.com>
2021-02-17 23:20:26 -0500

    inicio

---
+++ src/main/java/pe/com/bootcamp/BootcampChallenge1Application.java
@@ -0,0 +1,13 @@
+package pe.com.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampChallenge1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampChallenge1Application.class, args);
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+
+-- Inicializamos la BD
+
+
+INSERT INTO Tea (id_tea, tea, estado) VALUES ('1', '99.90%','1');
+INSERT INTO Tea (id_tea, tea, estado) VALUES ('1', '95.90%','1');
+INSERT INTO Tea (id_tea, tea, estado) VALUES ('1', '90.90%','1');
+INSERT INTO Tipocambio (id, valor) VALUES (1, 3.33);
+INSERT INTO Tipocambio (id, valor) VALUES (2, 3.74);
